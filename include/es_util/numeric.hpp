0de7e4e3554ee02c71843c83bbe39e809ea1b419
eugnsp <evgeny.sg@gmail.com>
2018-12-27 22:14:47 +0300

    Update

--- include/es_util/numeric.hpp
+++ include/es_util/numeric.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <es_util/numeric/abs.hpp>
+#include <es_util/numeric/const.hpp>
 #include <es_util/numeric/det.hpp>
 #include <es_util/numeric/even_odd.hpp>
 #include <es_util/numeric/power.hpp>
